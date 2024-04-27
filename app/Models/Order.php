1613c67e9f07fb616d0d70bacc9dd7ab0de21a9a
anounymous <anounymous@none.com>
2024-04-27 15:27:20 +0330

    feat(branch:feature/models-implementions) -> add Offer model, migration, factory and relation methods.

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Order extends Model
 {
@@ -15,5 +16,10 @@ class Order extends Model
     public function items():HasMany
     {
         return $this->hasMany(OrderItem::class);
+    }
+
+    public function offer():HasOne
+    {
+        return $this->hasOne(Offer::class);
     }
 }
---
+++ database/factories/OfferFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Order;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Offer>
+ */
+class OfferFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'order_id' => Order::factory(),
+            'remaining_amount' => $this->faker->randomFloat(),
+            'price' => rand(111111111, 999999999),
+        ];
+    }
+}
