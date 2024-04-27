34caddbd88361119c481123b74c18f9485ac83fd
anounymous <anounymous@none.com>
2024-04-27 15:14:29 +0330

    feat(branch:feature/models-implementions) -> add relation methods in exists models.

--- app/Models/Package.php
+++ app/Models/Package.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Package extends Model
 {
     use HasFactory;
+
+    public function type():HasOne
+    {
+        return $this->hasOne(PackageType::class);
+    }
+
+    public function service():BelongsTo
+    {
+        return $this->belongsTo(Service::class);
+    }
 }
