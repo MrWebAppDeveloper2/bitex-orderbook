35f034beb55d6e895f095558ab8e35fedcbe3374
anounymous <anounymous@none.com>
2024-04-27 18:49:05 +0330

    feat(branch:feature/offers-list-caching) -> assert OffersCacheListUpdater listener add new created offer to buy or sell cache list if the list is empty.

--- app/Listeners/OffersCacheListUpdater.php
+++ app/Listeners/OffersCacheListUpdater.php
@@ -3,12 +3,15 @@
 namespace App\Listeners;
 
 use App\Enums\Offer\OfferAtomLockName;
+use App\Enums\Offer\OfferCacheListName;
 use App\Enums\Offer\OfferType;
 use App\Events\OfferCreated;
 use App\Models\Offer;
 use Illuminate\Contracts\Cache\LockTimeoutException;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Cache;
 
 class OffersCacheListUpdater
 {
@@ -58,5 +61,17 @@ class OffersCacheListUpdater
         $this->offer = $event->offer;
 
         $lock = $this->getAtomLock();
+
+        $listName = $this->offer->type == OfferType::BUY->value?
+            OfferCacheListName::BUY_CACHE_LIST->value:
+            OfferCacheListName::SELL_CACHE_LIST->value;
+
+        $list = Cache::get($listName, new Collection());
+
+        if($list->isEmpty()){
+            $list->add($this->offer);
+
+            Cache::set($listName, $list);
+        }
     }
 }
--- tests/Feature/Listeners/OffersCacheListUpdaterTest.php
+++ tests/Feature/Listeners/OffersCacheListUpdaterTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature\Listeners;
 
 use App\Enums\Offer\OfferAtomLockName;
+use App\Enums\Offer\OfferCacheListName;
 use App\Events\OfferCreated;
 use App\Listeners\OffersCacheListUpdater;
 use App\Models\Offer;
@@ -10,6 +11,7 @@ use Illuminate\Cache\ArrayLock;
 use Illuminate\Contracts\Cache\LockTimeoutException;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Event;
@@ -18,6 +20,13 @@ use Tests\TestCase;
 
 class OffersCacheListUpdaterTest extends TestCase
 {
+    private function mockCacheFacadeForTestAtomLoc()
+    {
+        Cache::shouldReceive('get')->andReturn(new Collection());
+
+        Cache::shouldReceive('set');
+    }
+
     public function test_the_listener_listens_to_offer_created_event()
     {
         Event::fake();
@@ -29,6 +38,8 @@ class OffersCacheListUpdaterTest extends TestCase
 
     public function test_the_listener_request_for_atomic_lock_with_buy_offer_lock_key_with_specified_second_lock_time_in_the_config_when_new_offer_type_is_buy()
     {
+        $this->mockCacheFacadeForTestAtomLoc();
+
         $lockTime = config()->get('custom.offer.lock_time');
 
         Cache::shouldReceive('lock')
@@ -40,6 +51,8 @@ class OffersCacheListUpdaterTest extends TestCase
 
     public function test_the_listener_wait_for_release_buy_offer_type_atomic_lock_when_new_offer_type_is_buy_and_atomic_lock_is_not_free()
     {
+        $this->mockCacheFacadeForTestAtomLoc();
+
         $lockTime = config()->get('custom.offer.lock_time');
 
         $waitingTime = config()->get('custom.offer.waiting_time');
@@ -60,6 +73,8 @@ class OffersCacheListUpdaterTest extends TestCase
 
     public function test_the_listener_break_the_buy_offer_type_atomic_lock_and_force_release_it_when_after_maximum_waiting_timeout_when_new_offer_type_is_buy()
     {
+        $this->mockCacheFacadeForTestAtomLoc();
+
         $lockTime = config()->get('custom.offer.lock_time');
 
         $waitingTime = config()->get('custom.offer.waiting_time');
@@ -82,10 +97,25 @@ class OffersCacheListUpdaterTest extends TestCase
         Offer::factory()->buy()->create();
     }
 
+    public function test_the_listener_push_the_buy_type_new_created_offer_when_buy_offer_cache_list_is_empty()
+    {
+        $this->assertEmpty(Cache::get(OfferCacheListName::BUY_CACHE_LIST->value));
+
+        $offer = Offer::factory()->buy()->create();
+
+        $expectedCacheCollection = collect([$offer]);
+
+        $this->assertNotEmpty(Cache::get(OfferCacheListName::BUY_CACHE_LIST->value));
+
+        $this->assertEquals($expectedCacheCollection->toArray(), Cache::get(OfferCacheListName::BUY_CACHE_LIST->value)->toArray());
+    }
+
     // ------------------------- SELL OFFER TYPE TEST ----------------------------- //
 
     public function test_the_listener_request_for_atomic_lock_with_sell_offer_lock_key_with_specified_second_lock_time_in_the_config_when_new_offer_type_is_sell()
     {
+        $this->mockCacheFacadeForTestAtomLoc();
+
         $lockTime = config()->get('custom.offer.lock_time');
 
         Cache::shouldReceive('lock')
@@ -97,6 +127,8 @@ class OffersCacheListUpdaterTest extends TestCase
 
     public function test_the_listener_wait_for_release_sell_offer_type_atomic_lock_when_new_offer_type_is_sell_and_atomic_lock_is_not_free()
     {
+        $this->mockCacheFacadeForTestAtomLoc();
+
         $lockTime = config()->get('custom.offer.lock_time');
 
         $waitingTime = config()->get('custom.offer.waiting_time');
@@ -117,6 +149,8 @@ class OffersCacheListUpdaterTest extends TestCase
 
     public function test_the_listener_break_the_sell_offer_type_atomic_lock_and_force_release_it_when_after_maximum_waiting_timeout_when_new_offer_type_is_sell()
     {
+        $this->mockCacheFacadeForTestAtomLoc();
+
         $lockTime = config()->get('custom.offer.lock_time');
 
         $waitingTime = config()->get('custom.offer.waiting_time');
@@ -137,5 +171,18 @@ class OffersCacheListUpdaterTest extends TestCase
             ->andReturn($mockLock);
 
         Offer::factory()->sell()->create();
+    }
+
+    public function test_the_listener_push_the_sell_type_new_created_offer_when_sell_offer_cache_list_is_empty()
+    {
+        $this->assertEmpty(Cache::get(OfferCacheListName::SELL_CACHE_LIST->value));
+
+        $offer = Offer::factory()->sell()->create();
+
+        $expectedCacheCollection = collect([$offer]);
+
+        $this->assertNotEmpty(Cache::get(OfferCacheListName::SELL_CACHE_LIST->value));
+
+        $this->assertEquals($expectedCacheCollection->toArray(), Cache::get(OfferCacheListName::SELL_CACHE_LIST->value)->toArray());
     }
 }
