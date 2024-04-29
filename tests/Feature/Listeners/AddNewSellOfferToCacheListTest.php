1b9b7fc4c652183f567cf2ac06abf4aff630a399
anounymous <anounymous@none.com>
2024-04-29 19:36:56 +0330

    refactor(branch:feature/refactor-listeners) -> remove cache list limitation tester.

--- tests/Feature/Listeners/AddNewSellOfferToCacheListTest.php
+++ tests/Feature/Listeners/AddNewSellOfferToCacheListTest.php
@@ -118,41 +118,6 @@ class AddNewSellOfferToCacheListTest extends TestCase
         Event::assertDispatched(SellOffersCacheListUpdated::class);
     }
 
-    public function test_the_listener_add_new_sell_offer_to_cache_list_and_broadcast_it_when_list_length_is_smaller_than_maximum_length_limitation()
-    {
-        Event::fake();
-
-        $maximumLength = rand(5, 10);
-
-        config()->set('custom.offer.cache_list_length', $maximumLength);
-
-        $offers = Offer::factory()->sell()->count(($maximumLength - (int)ceil($maximumLength / 2)))->create();
-
-        $cacheList = $offers->map(function ($offer) {
-            return [
-                'price' => $offer->price,
-                'remaining_amount' => $offer->remaining_amount,
-            ];
-        })->toArray();
-
-        Cache::set(OfferCacheListName::SELL_CACHE_LIST->value, $cacheList);
-
-        $newOffer = Offer::factory()->sell()->create();
-
-        $listener = app()->make(AddNewSellOfferToCacheList::class);
-
-        $listener->handle(new OfferCreated($newOffer));
-
-        $cacheList[] = [
-            'price' => $newOffer->price,
-            'remaining_amount' => $newOffer->remaining_amount,
-        ];
-
-        $this->assertEqualsCanonicalizing(Cache::get(OfferCacheListName::SELL_CACHE_LIST->value), $cacheList);
-
-        Event::assertDispatched(SellOffersCacheListUpdated::class);
-    }
-
     public function test_the_listener_avoiding_update_sell_offers_cache_list_and_broadcast_it_when_the_list_is_not_empty_and_new_created_offer_type_is_sell_and_the_offer_has_not_lower_price_than_at_least_one_of_exists_offers()
     {
         Event::fake();
