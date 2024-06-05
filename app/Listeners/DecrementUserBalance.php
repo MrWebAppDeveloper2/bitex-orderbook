630c541f1a19279632fc3c69454fd5cb33a7c0fb
anounymous <anounymous@none.com>
2024-06-05 12:00:02 +0330

    refactor(dev) -> add order by price to find offer for trade quieries.

--- app/Listeners/DecrementUserBalance.php
+++ app/Listeners/DecrementUserBalance.php
@@ -13,7 +13,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
-class DecrementUserBalance
+class DecrementUserBalance implements ShouldQueue
 {
     /**
      * Create the event listener.
